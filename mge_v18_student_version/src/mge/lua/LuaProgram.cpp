2feb1a5343fc92630b8cbd743ef5b7e5f5b6544c
Igleer <milaqiigli@gmail.com>
2019-02-12 12:48:05 +0100

    Added Lua Program to the engine.

---
+++ mge_v18_student_version/src/mge/lua/LuaProgram.cpp
@@ -0,0 +1,167 @@
+//
+// Created by Igli milaqi on 15/01/2019.
+//
+
+#include "LuaProgram.h"
+#include <string>
+
+int (*Engine::LuaProgram::function)(lua_State* l) = nullptr;
+
+Engine::LuaProgram::LuaProgram(const std::string &filename)
+{
+    m_currentLuaState = GenerateProgram(filename);
+    //Test<int,int>([](int i) { std::cout<<"WORKS: "<< i <<std::endl; return 1;},2);
+
+    //if(function != nullptr)
+   //    function(m_currentLuaState);
+}
+
+lua_State *Engine::LuaProgram::GenerateProgram(const std::string &filename)
+{
+    lua_State* luaState = luaL_newstate();
+    luaL_openlibs(luaState);
+    luaL_loadfile(luaState,filename.c_str());  // convert the filename string to C style string
+
+    return luaState;
+}
+
+double Engine::LuaProgram::GetGlobalDouble(std::string varName)
+{
+    lua_getglobal(m_currentLuaState,varName.c_str());
+
+    if(lua_isnumber(m_currentLuaState,-1))
+    {
+        double d = lua_tonumber(m_currentLuaState,-1);
+        lua_pop(m_currentLuaState,1);
+        return d;
+    }
+    else {
+        std::cout<<"The global is not of type Double"<<std::endl;
+        throw;
+    }
+}
+
+int Engine::LuaProgram::GetGlobalBoolean(std::string varName)
+{
+    lua_getglobal(m_currentLuaState,varName.c_str());
+
+    if(lua_isboolean(m_currentLuaState,-1))
+    {
+        double b = lua_toboolean(m_currentLuaState,-1);
+        lua_pop(m_currentLuaState,1);
+        return b;
+    }
+    else {
+        std::cout<< "The global is not of type Boolean" <<std::endl;
+        throw;
+    }
+}
+
+int Engine::LuaProgram::GetGlobalInt(std::string varName)
+{
+    lua_getglobal(m_currentLuaState,varName.c_str());
+
+    if(lua_isinteger(m_currentLuaState,-1))
+    {
+        int i = lua_tointeger(m_currentLuaState,-1);
+        lua_pop(m_currentLuaState,1);
+        return  i;
+    }
+    else {
+        std::cout<<"The global is not of type Int"<<std::endl;
+        throw;
+    }
+}
+
+std::string Engine::LuaProgram::GetGlobalString(std::string varName)
+{
+    lua_getglobal(m_currentLuaState,varName.c_str());
+
+    if(lua_isstring(m_currentLuaState,-1))
+    {
+        std::string s = lua_tostring(m_currentLuaState,-1);
+        lua_pop(m_currentLuaState,1);
+        return s;
+    }
+    else {
+        std::cout<<"The global is not of type String"<<std::endl;
+        throw;
+    }
+}
+
+
+void Engine::LuaProgram::CallCurrentProgram()
+{
+    if(lua_pcall(m_currentLuaState,0,0,0) != 0)         //only if its zero it is called correctly
+    {
+        std::string error = lua_tostring(m_currentLuaState,-1);   //get the error from the top of the stack
+        std::cout<<error<<std::endl;
+        lua_pop(m_currentLuaState,1);
+        throw error;
+    }
+}
+
+void Engine::LuaProgram::SetGlobalDouble(std::string varName,double d)
+{
+    lua_pushnumber(m_currentLuaState,d);
+    lua_setglobal(m_currentLuaState,varName.c_str());
+}
+
+void Engine::LuaProgram::SetGlobalInt(std::string varName, int i)
+{
+    lua_pushinteger(m_currentLuaState,i);
+    lua_setglobal(m_currentLuaState,varName.c_str());
+}
+
+void Engine::LuaProgram::SetGlobalString(std::string varName, std::string s)
+{
+    lua_pushstring(m_currentLuaState,s.c_str());
+    lua_setglobal(m_currentLuaState,varName.c_str());
+}
+
+void Engine::LuaProgram::SetGlobalBool(std::string varName,bool b)
+{
+    lua_pushboolean(m_currentLuaState,b);
+    lua_setglobal(m_currentLuaState,varName.c_str());
+}
+
+void Engine::LuaProgram::GetGlobalFunction(std::string funcName, int NrOfArguments, int NrOfReturns)
+{
+    if(m_isFuncBeingCalled == true)
+    {
+        std::cout<<"You are already trying to get a function call that one first"<<std::endl;
+        throw;
+    }
+
+    m_totalArgumentNumber = NrOfArguments;
+    m_totalReturnNumber = NrOfReturns;
+    m_isFuncBeingCalled = true;
+    lua_getglobal(m_currentLuaState,funcName.c_str());
+}
+
+int Engine::LuaProgram::GetStackCount()
+{
+    return lua_gettop(m_currentLuaState);
+}
+
+void Engine::LuaProgram::CallGlobalFunction(std::string funcName)
+{
+    if(m_currentArgumentPassed > m_totalArgumentNumber)
+    {
+        std::cout<<"You have pushed more arguments then specified"<<std::endl;
+        throw;
+    }
+
+    lua_call(m_currentLuaState,m_totalArgumentNumber,m_totalReturnNumber);
+    m_isFuncBeingCalled = true;
+
+}
+
+void Engine::LuaProgram::CallFunctionInTable(std::string tableName, std::string fieldName)
+{
+    lua_getglobal(m_currentLuaState,tableName.c_str());
+    lua_getfield(m_currentLuaState,-1,fieldName.c_str());
+    lua_pcall(m_currentLuaState,0,0,0);
+}
+
+
---
+++ mge_v18_student_version/src/mge/lua/LuaProgram.h
@@ -0,0 +1,373 @@
+//
+// Created by Igli milaqi on 15/01/2019.
+//
+
+#ifndef LUAASSIGNMENT_LUAPROGRAM_H
+#define LUAASSIGNMENT_LUAPROGRAM_H
+
+#include <iostream>
+#include <lua.hpp>
+#include <tuple>
+#include <type_traits>
+
+namespace Engine {
+
+    class LuaProgram {
+
+    public:
+        LuaProgram(const std::string& filename);
+
+        double GetGlobalDouble(std::string varName);
+        int GetGlobalInt(std::string varName);
+        std::string GetGlobalString(std::string varName);
+        int GetGlobalBoolean(std::string varName);
+
+        int GetStackCount();
+        //All the set globals need to be set before the init call of the program.
+        //All the global values in lua have to be not initialized otherwise it will
+        //override our c++ value!!!
+        void SetGlobalDouble(std::string varName,double d);
+        void SetGlobalInt(std::string varName,int i);
+        void SetGlobalString(std::string varName,std::string s);
+        void SetGlobalBool(std::string varName,bool b);
+
+        void SetNewLibrary(const std::string& name,const struct luaL_Reg library[] )
+        {
+            luaL_newlib(m_currentLuaState,library);
+            lua_setglobal(m_currentLuaState,name.c_str());
+        }
+
+        void CallFunctionInTable(std::string tableName,std::string fieldName);
+
+        //calls the lua file. used for initializing.
+        void CallCurrentProgram();
+
+        void GetGlobalFunction(std::string funcName,int NrOfArguments, int NrOfReturns);
+        void CallGlobalFunction(std::string funcName);
+
+        template <typename T>
+        void PushFunctionArgument(T argument)
+        {
+            if(m_currentArgumentPassed >= m_totalArgumentNumber)
+            {
+                std::cout<<"You are passing more Arguments then specified"<<std::endl;
+                throw;
+            }
+
+            if constexpr (std::is_same<T, int>())
+            {
+                m_currentArgumentPassed +=1;
+                lua_pushinteger(m_currentLuaState, argument);
+            }
+            else if constexpr (std::is_same<T,std::string>())
+            {
+                m_currentArgumentPassed +=1;
+                lua_pushstring(m_currentLuaState, argument.c_str());
+            }
+            else if constexpr (std::is_same<T,double>())
+            {
+                m_currentArgumentPassed +=1;
+                lua_pushnumber(m_currentLuaState, argument);
+            }
+            else
+            {
+                std::cout<<"This type is not supported to be passed as an argument"<<std::endl;
+                throw;
+            }
+
+        }
+
+        template <typename T>
+        T PopFunctionReturn()
+        {
+            if(m_isFuncBeingCalled == false)
+            {
+                std::cout<<"there is no function called to access return values from!"<<std::endl;
+                throw;
+            }
+            if(m_currentReturnNumber == m_totalReturnNumber)
+            {
+                m_isFuncBeingCalled = false;
+                m_currentReturnNumber = 0;
+                m_currentArgumentPassed = 0;
+                m_totalReturnNumber = 0;
+                m_totalArgumentNumber = 0;
+            }
+            T returnType;
+
+            if constexpr (std::is_same<T, int>())
+            {
+                m_currentReturnNumber +=1;
+                returnType = lua_tointeger(m_currentLuaState,-1);
+                lua_pop(m_currentLuaState,1);
+                return  returnType;
+            }
+            else if constexpr (std::is_same<T, double>())
+            {
+                m_currentReturnNumber +=1;
+                returnType = lua_tonumber(m_currentLuaState,-1);
+                lua_pop(m_currentLuaState,1);
+                return  returnType;
+            }
+            else if constexpr (std::is_same<T, std::string>())
+            {
+                m_currentReturnNumber +=1;
+                returnType = lua_tostring(m_currentLuaState,-1);
+                lua_pop(m_currentLuaState,1);
+                return  returnType;
+            } else
+            {
+                std::cout<<"This type is not supported to be passed as an argument"<<std::endl;
+                throw;
+            }
+
+            return returnType;
+        }
+
+        template<typename T, typename U>
+        void PushToTable(const std::string& tableName,T key,U value)
+        {
+            lua_getglobal(m_currentLuaState,tableName.c_str());
+
+            if constexpr (std::is_same<T, int>())
+            {
+                lua_pushinteger(m_currentLuaState,key);
+                if constexpr (std::is_same<U, int>())
+                {
+                    lua_pushinteger(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    lua_pushnumber(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    lua_pushstring(m_currentLuaState,value.c_str());
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    lua_pushboolean(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U,  int(*)(lua_State*)>())
+                {
+                    lua_pushcfunction(m_currentLuaState,value);
+                }
+
+            }
+            else if constexpr (std::is_same<T, double>())
+            {
+                lua_pushnumber(m_currentLuaState,key);
+                if constexpr (std::is_same<U, int>())
+                {
+                    lua_pushinteger(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    lua_pushnumber(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    lua_pushstring(m_currentLuaState,value.c_str());
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    lua_pushboolean(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, int(*)(lua_State*)>())
+                {
+                    lua_pushcfunction(m_currentLuaState,value);
+                }
+            }
+            else if constexpr (std::is_same<T, std::string>())
+            {
+                lua_pushstring(m_currentLuaState,key.c_str());
+                if constexpr (std::is_same<U, int>())
+                {
+                    lua_pushinteger(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    lua_pushnumber(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    lua_pushstring(m_currentLuaState,value.c_str());
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    lua_pushboolean(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, int(*)(lua_State*)>())
+                {
+                    lua_pushcfunction(m_currentLuaState,value);
+                }
+            }
+            else if constexpr  (std::is_same<T,bool>())
+            {
+                lua_pushboolean(m_currentLuaState,key);
+                if constexpr (std::is_same<U, int>())
+                {
+                    lua_pushinteger(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    lua_pushnumber(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    lua_pushstring(m_currentLuaState,value.c_str());
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    lua_pushboolean(m_currentLuaState,value);
+                }
+                else if constexpr (std::is_same<U, int(*)(lua_State*)>())
+                {
+                    lua_pushcfunction(m_currentLuaState,value);
+                }
+            }
+            else
+            {
+                std::cout<<"This type is not supported to be passed as an argument"<<std::endl;
+                throw;
+            }
+
+            lua_settable(m_currentLuaState,-3);  // set all keys and values and pop them
+
+            lua_pop(m_currentLuaState,1);   //pop the table
+        }
+
+        template <typename T,typename  U>
+        U GetValueFromTable(const std::string& tableName, T key)
+        {
+            lua_getglobal(m_currentLuaState,tableName.c_str());
+            U returnValue;
+
+            if constexpr (std::is_same<T, int>())
+            {
+                lua_pushinteger(m_currentLuaState,key);
+                lua_gettable(m_currentLuaState,-2); // -1 is the table , -2 is the key
+
+                if constexpr (std::is_same<U, int>())
+                {
+                    returnValue = lua_tointeger(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    returnValue = lua_tonumber(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    returnValue = lua_tostring(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    returnValue = lua_toboolean(m_currentLuaState,-1); //result was pushed on top
+                }
+            }
+            else if constexpr (std::is_same<T, double>())
+            {
+                lua_pushnumber(m_currentLuaState,key);
+                lua_gettable(m_currentLuaState,-2); // -1 is the table , -2 is the key
+
+                if constexpr (std::is_same<U, int>())
+                {
+                    returnValue = lua_tointeger(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    returnValue = lua_tonumber(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    returnValue = lua_tostring(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    returnValue = lua_toboolean(m_currentLuaState,-1); //result was pushed on top
+                }
+            }
+            else if constexpr (std::is_same<T, std::string>())
+            {
+                lua_pushstring(m_currentLuaState,key.c_str());
+                lua_gettable(m_currentLuaState,-2); // -1 is the table , -2 is the key
+
+                if constexpr (std::is_same<U, int>())
+                {
+                    returnValue = lua_tointeger(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    returnValue = lua_tonumber(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    returnValue = lua_tostring(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    returnValue = lua_toboolean(m_currentLuaState,-1); //result was pushed on top
+                }
+            }
+            else if constexpr  (std::is_same<T,bool>())
+            {
+                lua_pushboolean(m_currentLuaState,key);
+                lua_gettable(m_currentLuaState,-2); // -1 is the table , -2 is the key
+
+                if constexpr (std::is_same<U, int>())
+                {
+                    returnValue = lua_tointeger(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, double>())
+                {
+                    returnValue = lua_tonumber(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, std::string>())
+                {
+                    returnValue = lua_tostring(m_currentLuaState,-1); //result was pushed on top
+                }
+                else if constexpr (std::is_same<U, bool>())
+                {
+                    returnValue = lua_toboolean(m_currentLuaState,-1); //result was pushed on top
+                }
+            }
+            else
+            {
+                std::cout<<"This type is not supported to be passed as an argument"<<std::endl;
+                throw;
+            }
+            return  returnValue;
+        }
+
+        static int (*function)(lua_State* l) ;
+
+        //TODO take a look here.
+        template <typename  T,typename U>
+        void Test(T (*pfunction) (U argument),U a )
+        {
+            auto lambda = [](lua_State* l)
+                    {
+                        //pfunction();
+                        std::cout<<"Hello"<<std::endl;
+                        return 1;
+                    };
+
+            function = lambda;
+        }
+
+    private:
+       lua_State* GenerateProgram(const std::string &filename);
+       lua_State* m_currentLuaState;
+
+       int m_totalArgumentNumber = 0;
+       int m_currentArgumentPassed = 0;
+       int m_totalReturnNumber = 0;
+       int m_currentReturnNumber = 0;
+       bool m_isFuncBeingCalled = false;
+
+
+    };
+}
+
+
+#endif //LUAASSIGNMENT_LUAPROGRAM_H
