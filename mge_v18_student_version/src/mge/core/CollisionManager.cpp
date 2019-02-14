3d742f6e78a70424d439790e801c912baab0b9c7
Ilyas <issatayev.ilyas@gmail.com>
2019-02-14 15:34:05 +0100

    Added map collisionLayer, now checking gameobjects id rather than collider id

--- mge_v18_student_version/src/mge/core/CollisionManager.cpp
+++ mge_v18_student_version/src/mge/core/CollisionManager.cpp
@@ -1,10 +1,32 @@
 #include "CollisionManager.h"
 
-int CollisionManager::AddCollider(ColliderComponent * newCollider)
+CollisionManager::CollisionManager()
 {
-	std::cout << "Add collider" << std::endl;
+	//Adding default layer "all", inlcuding all colliders
+	AddLayer("all");
+}
+
+void CollisionManager::AddLayer(std::string layerName)
+{
+	//Craeting new layer with empty vector of Colliders
+	m_collisionLayers[layerName] = new std::vector<ColliderComponent*>();
+}
+
+int CollisionManager::AddCollider(ColliderComponent * newCollider, std::string layerName)
+{
+	//Checking if layerName exists in m_collisionLayers
+	if (m_collisionLayers.count(layerName) > 0)
+	{
+		m_collisionLayers[layerName]->push_back(newCollider);
+		std::cout << "Collider added to collision layer: "<<layerName << std::endl;
+	}
+	else
+	{
+		std::cout << "There is no collision layer with name " << layerName << std::endl;
+		throw;
+	}
 	m_colliders.push_back(newCollider);
-	return m_colliders.size();
+	return 0;
 }
 
 bool CollisionManager::CheckCollisionInWorld(ColliderComponent * targetCollider)
@@ -12,10 +34,20 @@ bool CollisionManager::CheckCollisionInWorld(ColliderComponent * targetCollider)
 	for (int i = m_colliders.size() - 1; i >= 0; --i)
 	{
 		//if its not me and I am colliding with other object
-		if (targetCollider->id != m_colliders[i]->id  &&  targetCollider->IsColliding(m_colliders[i]))
+		if (targetCollider->GetGameObject()->ID() != m_colliders[i]->GetGameObject()->ID() &&  targetCollider->IsColliding(m_colliders[i]))
 		{
 			return true;
 		}
 	}
 	return false;
+	std::map<std::string, std::vector<ColliderComponent*>*>::iterator layerIterator;
+
+	for (layerIterator = m_collisionLayers.begin(); layerIterator != m_collisionLayers.end(); layerIterator++)
+	{
+		std::cout << layerIterator->first  // string (key)
+			<< ':'
+			<< layerIterator->second   // string's value 
+			<< std::endl;
+	}
+
 }
--- mge_v18_student_version/src/mge/core/CollisionManager.h
+++ mge_v18_student_version/src/mge/core/CollisionManager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <map>
+
 #include "../components/ColliderComponent.hpp"
 
 class ColliderComponent;
@@ -7,8 +9,13 @@ class ColliderComponent;
 class CollisionManager 
 {
 public:
-	int AddCollider(ColliderComponent* newCollider);
+	CollisionManager();
+	void AddLayer(std::string layerName);
+	int AddCollider(ColliderComponent* newCollider, std::string layerName="all");
 	bool CheckCollisionInWorld(ColliderComponent* targetCollider);
+
+
 private:
 	std::vector<ColliderComponent*> m_colliders;
+	std::map<std::string, std::vector<ColliderComponent*>*> m_collisionLayers;
 };
